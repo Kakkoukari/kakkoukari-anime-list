843775308fd9fe6e54d01b56d4b3561376155c4f
metal-oopa <sudip5banerjee1974@gmail.com>
2022-08-21 21:08:55 +0530

    change new anime

--- anime/routes/anime/new.ts
+++ anime/routes/anime/new.ts
@@ -12,37 +12,48 @@ router.post(
   "/api/anime/new",
   requireAuth,
   async (req: Request, res: Response) => {
-    const response = await axios({
+    let page = 1;
+
+    let response = await axios({
       method: "get",
-      url: `https://api.jikan.moe/v4/anime`,
+      url: `https://api.jikan.moe/v4/anime?page=${page}`,
     });
-    let size = response.data.data.length - 1;
-    let count = 0;
+    page = response.data.pagination.last_visible_page;
     let flag = true;
+    let count = 0;
     const animesAdded = [];
 
     while (flag) {
-      const animeDetails = response.data.data[size--];
-      const anime = await Anime.findOne({ malId: animeDetails.mal_id });
+      response = await axios({
+        method: "get",
+        url: `https://api.jikan.moe/v4/anime?page=${page}`,
+      });
 
-      if (anime) {
-        flag = false;
-      } else {
-        const newAnime = new Anime({
-          titles: animeDetails.titles,
-          type: animeDetails.type,
-          malId: animeDetails.mal_id,
-          images: animeDetails.images.jpg.image_url,
-          episodes: animeDetails.episodes,
-          duration: animeDetails.duration,
-          rating: animeDetails.rating,
-          score: animeDetails.score,
-          synopsis: animeDetails.synopsis,
-          genres: animeDetails.genres,
-        });
-        await newAnime.save();
-        animesAdded.push(newAnime);
-        count++;
+      let size = response.data.data.length - 1;
+
+      while (size >= 0 && flag) {
+        const animeDetails = response.data.data[size--];
+        const anime = await Anime.findOne({ malId: animeDetails.mal_id });
+
+        if (anime) {
+          flag = false;
+        } else {
+          const newAnime = new Anime({
+            titles: animeDetails.titles,
+            type: animeDetails.type,
+            malId: animeDetails.mal_id,
+            images: animeDetails.images.jpg.image_url,
+            episodes: animeDetails.episodes,
+            duration: animeDetails.duration,
+            rating: animeDetails.rating,
+            score: animeDetails.score,
+            synopsis: animeDetails.synopsis,
+            genres: animeDetails.genres,
+          });
+          await newAnime.save();
+          animesAdded.push(newAnime);
+          count++;
+        }
       }
     }
 
