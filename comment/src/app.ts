677c12ac2959335ba0aa85e9a27e584979880a20
Debajyoti <debajyotidhar111@gmail.com>
2022-08-25 18:51:39 +0530

    Auth and User update

---
+++ comment/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@devion/common";
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    name:"session",
+    keys: [process.env.JWT_KEY!],
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
