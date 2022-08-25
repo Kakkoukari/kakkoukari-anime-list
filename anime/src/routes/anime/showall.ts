1ba82ec40511a5511ff1f5972e65ca86e760c17a
metal-oopa <sudip5banerjee1974@gmail.com>
2022-08-25 17:24:23 +0530

    update

--- anime/src/routes/anime/showall.ts
+++ anime/src/routes/anime/showall.ts
@@ -4,7 +4,8 @@ import { Anime } from "../../models/anime";
 const router = express.Router();
 
 router.get("/api/anime", async (req: Request, res: Response) => {
-  const anime = Anime.find();
+  const anime = await Anime.find();
+  console.log("anime");
   res.send(anime);
 });
 
