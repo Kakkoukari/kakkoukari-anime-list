2f6dc11e3b6f8ee9ea7c8cfd11f70c5b5e7a3d45
Debajyoti <debajyotidhar111@gmail.com>
2022-09-24 22:43:55 +0530

    Added comment service routes

---
+++ comment/src/routes/add-comment.ts
@@ -0,0 +1,25 @@
+import express,{Request, Response} from "express";
+import { currentUser } from "@devion/common";
+import {Comment} from "../models/Comment.ts";
+const router = express.Router();
+
+router.post("/api/comments/add", currentUser, async (req:Request, res:Response)=>{
+    const {content, animeId, malId} = req.body;
+    console.log("Checking in add comment route", content);
+
+    const addedComment = Comment.build({
+        content: content,
+        animeId: animeId,
+        malId: malId,
+        userId: req.currentUser!.id
+    });
+
+    await addedComment.save();
+
+    console.log("The newly added comment is:\n", addedComment);
+
+    res.sendStatus(201).send("Comment Added Successfully!");
+})
+
+
+export {router as AddCommentRouter};
