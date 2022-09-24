ad55cd3eb6306758f1cff0dc8ac292487831822a
metal-oopa <sudip5banerjee1974@gmail.com>
2022-09-24 22:35:59 +0530

    add comment model

---
+++ comment/src/models/Comment.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+interface CommentAttrs {
+  content?: string;
+  animeId?: string;
+  userId?: string;
+}
+
+interface CommentModel extends mongoose.Model<CommentDoc> {
+  build(attrs: CommentAttrs): CommentDoc;
+}
+
+export interface CommentDoc extends mongoose.Document {
+  content?: string;
+  animeId?: string;
+  userId?: string;
+}
+
+const CommentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: false,
+    },
+    animeId: {
+      type: String,
+      required: false,
+    },
+    userId: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc: any, ret: any) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+    timestamps: true,
+  }
+);
+
+CommentSchema.statics.build = (attrs: CommentAttrs) => {
+  return new Comment(attrs);
+};
+
+const Comment = mongoose.model<CommentDoc, CommentModel>(
+  "Comment",
+  CommentSchema
+);
+
+export { Comment };
