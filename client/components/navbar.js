577cd09248c187ebdc2c6645e0d8e6c19b640a31
Debajyoti <debajyotidhar111@gmail.com>
2022-09-30 01:25:27 +0530

    Fixed CSS and props

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "../styles/navbar.module.scss";
 import Link from "next/link";
+import Router from "next/router";
 const NavBar = ({ currentUser }) => (
   <header className={styles.navbar}>
-    <div className={`${styles.navbar__item} ${styles.navbar__title} `}>KAL</div>
+    <div className={`${styles.navbar__item} ${styles.navbar__title} `}>
+      <a href="/">KAL</a>
+    </div>
     <div className={`${styles.navbar__item} ${styles.navbar__link}`}>
       {currentUser ? (
         <Link href="#">
